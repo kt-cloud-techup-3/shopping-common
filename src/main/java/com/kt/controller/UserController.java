11d0fcebabc584219d891383c9639ff39df8a5c7
junsang-ee <dkwnstkddk@gmail.com>
2025-11-03 17:15:10 +0900

    refactor: 공통 URL endPoint -> RequestMapping 으로 정의, SpringDoc(swagger, ApiResponse) 클래스 단위로 이동 및 정의

--- src/main/java/com/kt/controller/UserController.java
+++ src/main/java/com/kt/controller/UserController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,17 +18,19 @@ import com.kt.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "유저", description = "유저 관련 API")
-@RestController
+@ApiResponses(value = {
+	@ApiResponse(responseCode = "400", description = "유효성 검사 실패"),
+	@ApiResponse(responseCode = "500", description = "서버 에러 - 백엔드에 바로 문의 바랍니다.")
+})
+@RequestMapping("/users")
 @RequiredArgsConstructor
+@RestController
 public class UserController {
 
 	private final UserService userService;
 
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "400", description = "유효성 검사 실패"),
-		@ApiResponse(responseCode = "500", description = "서버 에러 - 백엔드에 바로 문의 바랍니다.")
-	})
-	@PostMapping("/users")
+
+	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void create(@Valid @RequestBody UserCreateRequest request) {
 		userService.create(request);
