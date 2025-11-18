df5869c2f6e7fd7ac3ba7a5e52cc83643df90780
junsang <89866144+junsang-ee@users.noreply.github.com>
2025-11-18 21:54:19 +0900

    UserRole.java 업데이트
    UserRole Enum String Description 추가

--- src/main/java/com/kt/domain/constant/UserRole.java
+++ src/main/java/com/kt/domain/constant/UserRole.java
@@ -1,7 +1,11 @@
 package com.kt.domain.constant;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum UserRole {
-	ADMIN,
-	MEMBER,
-	COURIER
+	ADMIN("관리자"),
+	MEMBER("회원"),
+	COURIER("기사");
+    private final String description;
 }
