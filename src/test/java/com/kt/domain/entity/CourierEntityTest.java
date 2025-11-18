fa42f4d7ccfbe58636296461bd9cb1f2e2a7ae02
kf16 <wjdtn747@naver.com>
2025-11-19 08:08:32 +0900

    feat: CourierEntity 테스트 추가 ( CourierEntityTest )

---
+++ src/test/java/com/kt/domain/entity/CourierEntityTest.java
@@ -0,0 +1,26 @@
+package com.kt.domain.entity;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.kt.domain.constant.Gender;
+
+@ActiveProfiles("test")
+class CourierEntityTest {
+	@Test
+	void 객체생성_성공(){
+		CourierEntity courierEntity = CourierEntity.create(
+			"테스터1",
+			"wjd1234@naver.com",
+			"1234",
+			Gender.MALE
+		);
+		assertThat(courierEntity.getName()).isEqualTo("테스터1");
+		assertThat(courierEntity.getEmail()).isEqualTo("wjd1234@naver.com");
+		assertThat(courierEntity.getPassword()).isEqualTo("1234");
+		assertThat(courierEntity.getGender()).isEqualTo(Gender.MALE);
+	}
+}
