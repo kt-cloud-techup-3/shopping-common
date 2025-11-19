a345d16947cff51a4ffc8000bfaa117c98f8ef89
terry <tryterry77@gmail.com>
2025-11-19 20:12:25 +0900

    feat: repository id 타입 변경

--- src/main/java/com/kt/repository/ProductRepository.java
+++ src/main/java/com/kt/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.kt.repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,11 +10,9 @@ import com.kt.domain.entity.ProductEntity;
 import com.kt.exception.BaseException;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
 
 	default ProductEntity findByIdOrThrow(UUID productId) {
 		return findById(productId).orElseThrow(() -> new BaseException(ErrorCode.PRODUCT_NOT_FOUND));
 	}
-
-	Optional<ProductEntity> findById(UUID productId);
 }
