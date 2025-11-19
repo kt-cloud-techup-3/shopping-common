964a2c6e4c1bb31fbb9deb656de3ffcb58c3afa3
l-lyun <kimdohyun032@gmail.com>
2025-11-19 09:44:27 +0900

    feat: 장바구니 도메인 작성

---
+++ src/main/java/com/kt/domain/entity/CartEntity.java
@@ -0,0 +1,47 @@
+package com.kt.domain.entity;
+
+import com.kt.domain.entity.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "cart")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartEntity extends BaseEntity {
+	@Column(nullable = false)
+	private Long quantity;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	@Column(nullable = false)
+	private UserEntity userId;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	@Column(nullable = false)
+	private ProductEntity productId;
+
+	protected CartEntity(
+		Long quantity,
+		UserEntity userId,
+		ProductEntity productId
+	) {
+		this.quantity = quantity;
+		this.userId = userId;
+		this.productId = productId;
+	}
+
+	public static CartEntity create(
+		final Long quantity,
+		final UserEntity userId,
+		final ProductEntity productId
+	) {
+		return new CartEntity(quantity, userId, productId);
+	}
+}
