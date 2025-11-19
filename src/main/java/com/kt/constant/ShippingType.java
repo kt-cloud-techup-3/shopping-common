ecba291a55eb8bef24f8345082268f2aad8a499b
l-lyun <kimdohyun032@gmail.com>
2025-11-19 11:53:31 +0900

    fix: 반품 refund -> return

--- src/main/java/com/kt/constant/ShippingType.java
+++ src/main/java/com/kt/constant/ShippingType.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ShippingType {
 	DELIVERING("상품배송"),
-	REFUNDING("반품배송");
+	RETURNING("반품배송");
 
 	private final String description;
 }
