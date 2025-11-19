44403faefbb3eb7c113ed4efb0b55b0f783c5269
kf16 <wjdtn747@naver.com>
2025-11-19 12:36:27 +0900

    feat: CartEntity 테스트 추가 ( CartEntityTest )

--- src/test/java/com/kt/domain/entity/CartEntityTest.java
+++ src/test/java/com/kt/domain/entity/CartEntityTest.java
@@ -1,7 +1,50 @@
 package com.kt.domain.entity;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.kt.constant.Gender;
+import com.kt.constant.ProductStatus;
+import com.kt.constant.UserRole;
 
 class CartEntityTest {
 
+	UserEntity testUser;
+	ProductEntity testProduct;
+	@BeforeEach
+	void setUp() throws Exception {
+		testUser = UserEntity.create(
+			"주문자테스터1",
+			"wjd123@naver.com",
+			"1234",
+			UserRole.MEMBER,
+			Gender.MALE,
+			LocalDate.of(1990, 1, 1),
+			"010-1234-5678"
+		);
+		testProduct = ProductEntity.create(
+			"테스트상품명",
+			1000L,
+			5L,
+			ProductStatus.ACTIVATED
+		);
+	}
+
+	@Test
+	void 객체생성_성공(){
+		CartEntity cart = CartEntity.create(
+			5L,
+			testUser,
+			testProduct
+		);
+		assertThat(cart.getUser()).isEqualTo(testUser);
+		assertThat(cart.getProduct()).isEqualTo(testProduct);
+		assertThat(cart.getQuantity()).isEqualTo(5L);
+	}
 }
