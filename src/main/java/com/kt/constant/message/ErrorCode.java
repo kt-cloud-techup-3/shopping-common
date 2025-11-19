3194a32b2e581ae0f60d0e58625957f02b451af3
kf16 <wjdtn747@naver.com>
2025-11-19 22:08:20 +0900

    feat: Review Service Test 코드 작성

--- src/main/java/com/kt/constant/message/ErrorCode.java
+++ src/main/java/com/kt/constant/message/ErrorCode.java
@@ -2,6 +2,7 @@ package com.kt.constant.message;
 
 import java.text.MessageFormat;
 
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
@@ -13,7 +14,8 @@ public enum ErrorCode {
 	INVALID_DOMAIN_FIELD(HttpStatus.BAD_REQUEST, "도메인 필드 오류 : {0}"),
 	BODY_FIELD_ERROR(HttpStatus.BAD_REQUEST, "바디 필드 오류 : {0}"),
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
-	ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 주문은 존재하지 않습니다.");
+	ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 주문은 존재하지 않습니다."),
+	REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 리뷰입니다.");
 
 	private final HttpStatus status;
 	private final String message;
---
+++ src/test/java/com/kt/service/ReviewServiceImplTest.java
@@ -0,0 +1,137 @@
+package com.kt.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kt.constant.Gender;
+import com.kt.constant.OrderProductStatus;
+import com.kt.constant.ProductStatus;
+import com.kt.constant.ReviewStatus;
+import com.kt.constant.UserRole;
+import com.kt.constant.message.ErrorCode;
+import com.kt.domain.entity.OrderEntity;
+import com.kt.domain.entity.OrderProductEntity;
+import com.kt.domain.entity.ProductEntity;
+import com.kt.domain.entity.ReceiverVO;
+import com.kt.domain.entity.ReviewEntity;
+import com.kt.domain.entity.UserEntity;
+import com.kt.exception.BaseException;
+import com.kt.repository.OrderProductRepository;
+import com.kt.repository.OrderRepository;
+import com.kt.repository.ProductRepository;
+import com.kt.repository.ReviewRepository;
+import com.kt.repository.UserRepository;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class ReviewServiceImplTest {
+
+	@Autowired
+	ReviewService reviewService;
+	@Autowired
+	ReviewRepository reviewRepository;
+
+	@Autowired
+	OrderProductRepository orderProductRepository;
+	@Autowired
+	ProductRepository productRepository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	OrderRepository orderRepository;
+
+
+	OrderProductEntity testOrderProduct;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		UserEntity user = UserEntity.create(
+			"주문자테스터1",
+			"wjd123@naver.com",
+			"1234",
+			UserRole.MEMBER,
+			Gender.MALE,
+			LocalDate.now(),
+			"010-1234-5678"
+		)	;
+		userRepository.save(user);
+
+		ReceiverVO receiver = new ReceiverVO(
+			"수신자테스터1",
+			"010-1234-5678",
+			"강원도",
+			"원주시",
+			"행구로",
+			"주소설명"
+		);
+
+		OrderEntity order = OrderEntity.create(
+			receiver,
+			user
+		);
+		orderRepository.save(order);
+
+		ProductEntity product= ProductEntity.create(
+			"테스트상품명",
+			1000L,
+			5L,
+			ProductStatus.ACTIVATED
+		);
+		productRepository.save(product);
+
+		testOrderProduct = new OrderProductEntity(
+			5L,
+			5000L,
+			OrderProductStatus.CREATED,
+			order,
+			product
+		);
+		orderProductRepository.save(testOrderProduct);
+	}
+
+	@Test
+	void 리뷰생성_성공(){
+		reviewService.create(testOrderProduct.getId(),"테스트리뷰내용");
+
+		ReviewEntity foundedReviewEntity = reviewRepository
+			.findAll()
+			.stream()
+			.findFirst()
+			.orElseThrow(() -> new BaseException(ErrorCode.REVIEW_NOT_FOUND));
+
+		Assertions.assertNotNull(foundedReviewEntity);
+		Assertions.assertEquals(testOrderProduct.getId(),foundedReviewEntity.getOrderProduct().getId());
+	}
+
+	@Test
+	void 리뷰변경_성공(){
+		ReviewEntity reviewEntity = ReviewEntity.create("테스트리뷰내용");
+		reviewEntity.mapToOrderProduct(testOrderProduct);
+		reviewRepository.save(reviewEntity);
+
+		reviewService.update(reviewEntity.getId(),"변경된테스트리뷰내용");
+
+		Assertions.assertEquals("변경된테스트리뷰내용", reviewEntity.getContent());
+	}
+
+	@Test
+	void 리뷰삭제_성공(){
+		ReviewEntity reviewEntity = ReviewEntity.create("테스트리뷰내용");
+		reviewEntity.mapToOrderProduct(testOrderProduct);
+		reviewRepository.save(reviewEntity);
+		reviewService.delete(reviewEntity.getId());
+		Assertions.assertEquals(ReviewStatus.REMOVED, reviewEntity.getStatus());
+	}
+}
