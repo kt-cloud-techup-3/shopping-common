ff6622b9a6cbf5274596230e5e5afcc909511bf0
l-lyun <kimdohyun032@gmail.com>
2025-11-20 16:28:23 +0900

    feat: 내 주문 목록 조회

--- src/main/java/com/kt/service/UserServiceImpl.java
+++ src/main/java/com/kt/service/UserServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.kt.dto.response.OrderProductResponse;
 import com.kt.repository.OrderProductRepository;
+import com.kt.domain.dto.response.UserResponse;
+import com.kt.domain.entity.OrderEntity;
+import com.kt.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +20,13 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 
 	private final OrderProductRepository orderProductRepository;
+	private final OrderRepository orderRepository;
+
+	public UserResponse.Orders getOrdersByUserId(UUID id) {
+		List<OrderEntity> orders = orderRepository.findAllByOrderBy_Id(id);
+
+		return UserResponse.Orders.of(id, orders);
+	}
 
 	@Override
 	public List<OrderProductResponse.SearchReviewable> getReviewableOrderProducts(UUID userId) {
