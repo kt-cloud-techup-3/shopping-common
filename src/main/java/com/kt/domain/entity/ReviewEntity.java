bb751aa800b79e82beb27a7f891840c22e4a23eb
TopOfHead <164000692+FA-50@users.noreply.github.com>
2025-11-20 14:18:28 +0900

    [FEAT] Review 도메인 생성 및 테스트 코드 작성
    * fix: PaymentEntity 테스트코드 작성오류 수정 ( PaymentEntityTest )
    
    * feat : Review 도메인 생성 및 테스트 성공 , 실패 코드 작성 ( ReviewEntity , ReviewEntityTest , ReviewStatus )
    
    * refactor : 오타 수정

---
+++ src/main/java/com/kt/domain/entity/ReviewEntity.java
@@ -0,0 +1,54 @@
+package com.kt.domain.entity;
+
+import static lombok.AccessLevel.*;
+
+import org.apache.el.util.Validation;
+
+import com.kt.constant.ReviewStatus;
+import com.kt.domain.entity.common.BaseEntity;
+import com.kt.util.ValidationUtil;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "review")
+@NoArgsConstructor(access = PROTECTED)
+public class ReviewEntity extends BaseEntity {
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private ReviewStatus status;
+
+	@Column(nullable = false)
+	private String content;
+
+	@OneToOne
+	@JoinColumn(name = "order_product_id", nullable = false)
+	private OrderProductEntity orderProduct;
+
+	protected ReviewEntity(
+		ReviewStatus status,
+		String content
+	) {
+		ValidationUtil.validationNotNullEnum(status,"리뷰상태");
+		ValidationUtil.validateNotNullAndBlank(content,"내용");
+		this.status = status;
+		this.content = content;
+	}
+
+	public static ReviewEntity create(
+		final ReviewStatus status,
+		final String content
+	){
+		return new ReviewEntity(
+			status,
+			content
+		);
+	}
+}
