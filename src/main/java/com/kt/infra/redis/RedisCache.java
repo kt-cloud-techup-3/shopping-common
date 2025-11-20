333dfbdf7b364c7850da31c4f8cb65ec5c09bb84
junsang-ee <dkwnstkddk@gmail.com>
2025-11-21 03:17:07 +0900

    feat: redis 의존성 모듈 추가, Redis configuration, properties 및 redis wrapper 컴포넌트 생성 (application.yml redis 정보 추가)

---
+++ src/main/java/com/kt/infra/redis/RedisCache.java
@@ -0,0 +1,38 @@
+package com.kt.infra.redis;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedisCache {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public <T> void set(String key, T value) {
+		redisTemplate.opsForValue().set(key, value);
+	}
+
+	public <T> void set(String key, T value, Duration ttl) {
+		redisTemplate.opsForValue().set(key, value, ttl.toMillis(), TimeUnit.MILLISECONDS);
+	}
+
+	public <T> T get(String key, Class<T> clazz) {
+		Object value = redisTemplate.opsForValue().get(key);
+		return value != null ? clazz.cast(value) : null;
+	}
+
+	public void delete(String key) {
+		redisTemplate.delete(key);
+	}
+
+	public boolean hasKey(String key) {
+		return redisTemplate.hasKey(key);
+	}
+
+}
