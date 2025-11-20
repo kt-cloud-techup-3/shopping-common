eb30b49ca7eaba8c955df7294791fe58b06c3345
junsang-ee <dkwnstkddk@gmail.com>
2025-11-20 09:59:22 +0900

    feat: 	1. 유저 회원가입 (create userEntity) 서비스 로직 추가 및 테스트 코드 추가 (add AuthService)

---
+++ src/main/java/com/kt/service/AuthService.java
@@ -0,0 +1,9 @@
+package com.kt.service;
+
+import com.kt.domain.dto.request.MemberSignupRequest;
+
+public interface AuthService {
+
+	void memberSignup(MemberSignupRequest request);
+
+}
---
+++ src/test/java/com/kt/domain/service/AuthServiceTest.java
@@ -0,0 +1,79 @@
+package com.kt.domain.service;
+
+import com.kt.ShoppingApplication;
+import com.kt.constant.Gender;
+import com.kt.domain.dto.request.MemberSignupRequest;
+import com.kt.domain.entity.UserEntity;
+import com.kt.repository.UserRepository;
+
+import com.kt.service.AuthServiceImpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest(classes = ShoppingApplication.class)
+public class AuthServiceTest {
+
+	@Autowired AuthServiceImpl authService;
+
+	@Autowired UserRepository userRepository;
+
+
+	@Test
+	void 맴버_회원가입_성공_테스트() {
+		MemberSignupRequest signup = new MemberSignupRequest(
+			"황테스터",
+			"test@email.com",
+			"1231231!",
+			Gender.MALE,
+			LocalDate.of(2011, 11, 11),
+			"010-1234-1234"
+		);
+		authService.memberSignup(signup);
+		UserEntity member = userRepository.findByEmail(signup.email()).orElseGet(
+			() -> null
+		);
+		assertNotNull(member);
+	}
+
+	@Test
+	void 맴버_회원가입_실패_email_중복() {
+		MemberSignupRequest firstSignup = new MemberSignupRequest(
+			"황테스터1",
+			"test@email.com",
+			"1231231!",
+			Gender.MALE,
+			LocalDate.of(2011, 11, 10),
+			"010-1234-0001"
+		);
+
+		MemberSignupRequest secondSignup = new MemberSignupRequest(
+			"황테스터2",
+			"test@email.com",
+			"1231231!",
+			Gender.MALE,
+			LocalDate.of(2011, 11, 11),
+			"010-1234-0002"
+		);
+		authService.memberSignup(firstSignup);
+		UserEntity member = userRepository.findByEmail(firstSignup.email()).orElseGet(
+			() -> null
+		);
+		assertNotNull(member);
+
+		assertThrowsExactly(
+			IllegalArgumentException.class, () ->
+				authService.memberSignup(secondSignup)
+		);
+
+	}
+
+
+}
