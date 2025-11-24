f3f15b6939ff2fa7f951d8e092a0887634536785
junsang-ee <dkwnstkddk@gmail.com>
2025-11-24 17:14:05 +0900

    feat: 	1. 비밀번호 초기화 서비스 로직 개발 및 테스트 코드 추가 (성공 및 추가) 	2. MailTemplate (비밀번호 변경 안내) Enum (문구) 정의 	3. resetPassword (AbstractAccountEnrtity) 메소드 추가 	4. 테스트 코드 : foundedUser -> savedUser 변수명 (founded -> saved) 수정 및 통일

--- src/main/java/com/kt/constant/mail/MailTemplate.java
+++ src/main/java/com/kt/constant/mail/MailTemplate.java
@@ -8,7 +8,11 @@ import lombok.RequiredArgsConstructor;
 public enum MailTemplate {
 
 	VERIFY_EMAIL("TECHUP-SHOPPING 이메일 주소 인증",
-		"인증 코드를 입력하여 이메일 주소를 인증해 주세요.\n이 인증 코드는 발급 후 3분 동안만 유효합니다.");
+		"인증 코드를 입력하여 이메일 주소를 인증해 주세요.\n이 인증 코드는 발급 후 3분 동안만 유효합니다."),
+
+	RESET_PASSWORD("TECHUP-SHOPPING 비밀번호 변경 안내",
+		"임시 비밀번호를 발급하였습니다.\n임시 비밀번호로 로그인 하시기 바랍니다.");
+
 
 
 	private final String subject;
