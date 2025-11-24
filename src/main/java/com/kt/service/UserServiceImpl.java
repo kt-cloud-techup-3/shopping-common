21474db8e740ca9b170ce87bad847250c30e6ffe
kf16 <wjdtn747@naver.com>
2025-11-24 23:08:17 +0900

    feat: 유저서비스 - 비밀번호 변경 / 삭제 / 정보수정 기능 추가 및 테스트 코드 추가

--- src/main/java/com/kt/service/UserServiceImpl.java
+++ src/main/java/com/kt/service/UserServiceImpl.java
@@ -10,11 +10,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kt.constant.UserRole;
+import com.kt.constant.message.ErrorCode;
+import com.kt.domain.dto.request.UserRequest;
 import com.kt.domain.dto.request.SignupRequest;
 import com.kt.domain.dto.response.OrderProductResponse;
 import com.kt.domain.dto.response.UserResponse;
 import com.kt.domain.entity.OrderEntity;
 import com.kt.domain.entity.UserEntity;
+import com.kt.exception.AuthException;
+import com.kt.exception.DuplicatedException;
 import com.kt.repository.OrderProductRepository;
 import com.kt.repository.OrderRepository;
 import com.kt.repository.user.UserRepository;
@@ -113,4 +117,39 @@ public class UserServiceImpl implements UserService {
 		userRepository.save(admin);
 	}
 
+
+	@Override
+	public void updatePassword(
+		UUID userId,
+		String currentPassword ,
+		String newPassword)
+	{
+		UserEntity user = userRepository.findByIdOrThrow(userId);
+
+		if ( newPassword.equals(currentPassword) )
+			throw new DuplicatedException(ErrorCode.DUPLICATED_PASSWORD);
+
+		if ( !passwordEncoder.matches(currentPassword,user.getPassword()) )
+			throw new AuthException(ErrorCode.NOT_CORRECT_PASSWORD);
+
+		String hashedPassword = passwordEncoder.encode(newPassword);
+		user.updatePassword(hashedPassword);
+	}
+
+	@Override
+	public void delete(UUID userId) {
+		UserEntity user = userRepository.findByIdOrThrow(userId);
+		user.delete();
+	}
+
+	@Override
+	public void updateUserDetails(UUID userId, UserRequest.UpdateDetails details) {
+		UserEntity user = userRepository.findByIdOrThrow(userId);
+		user.updateDetails(
+			details.name(),
+			details.mobile(),
+			details.birth(),
+			details.gender()
+		);
+	}
 }
