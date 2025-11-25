866f01d75ec39a95725d4cc89f7cbc432c3c74f6
l-lyun <kimdohyun032@gmail.com>
2025-11-25 19:37:33 +0900

    feat: OrderRepository clearUser 메서드 작성

--- src/main/java/com/kt/repository/OrderRepository.java
+++ src/main/java/com/kt/repository/OrderRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kt.domain.entity.OrderEntity;
@@ -11,4 +13,8 @@ import com.kt.domain.entity.OrderEntity;
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
 	List<OrderEntity> findAllByOrderBy_Id(UUID id);
+
+	@Modifying
+	@Query("UPDATE OrderEntity o SET o.orderBy = NULL WHERE o.orderBy.id = :userId")
+	void clearUser(UUID userId);
 }
