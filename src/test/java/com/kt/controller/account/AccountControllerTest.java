42740d1e9dabc1fcda217e5892b4ae47488ea043
kf16 <wjdtn747@naver.com>
2025-11-26 20:56:51 +0900

    feat: AccountControllerTest 회원탈퇴 테스트 추가

--- src/test/java/com/kt/controller/account/AccountControllerTest.java
+++ src/test/java/com/kt/controller/account/AccountControllerTest.java
@@ -1,8 +1,8 @@
 package com.kt.controller.account;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 
 import java.time.LocalDate;
 
@@ -22,11 +22,15 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kt.constant.Gender;
 import com.kt.constant.UserRole;
+import com.kt.constant.UserStatus;
 import com.kt.domain.dto.request.AccountRequest;
 import com.kt.domain.entity.AbstractAccountEntity;
+import com.kt.domain.entity.CourierEntity;
 import com.kt.domain.entity.UserEntity;
 import com.kt.repository.AccountRepository;
+import com.kt.repository.courier.CourierRepository;
 import com.kt.repository.user.UserRepository;
+import com.kt.security.DefaultCurrentUser;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -38,6 +42,8 @@ class AccountControllerTest {
 	@Autowired
 	UserRepository userRepository;
 	@Autowired
+	CourierRepository courierRepository;
+	@Autowired
 	PasswordEncoder passwordEncoder;
 	@Autowired
 	MockMvc mockMvc;
@@ -46,6 +52,7 @@ class AccountControllerTest {
 
 	static final String TEST_PASSWORD = "1234567891011";
 	UserEntity testUser;
+	CourierEntity testCourier;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -59,6 +66,14 @@ class AccountControllerTest {
 			"010-1234-5678"
 		);
 		userRepository.save(testUser);
+
+		testCourier = CourierEntity.create(
+			"배송기사테스터",
+			"courier@naver.com",
+			passwordEncoder.encode(TEST_PASSWORD),
+			Gender.MALE
+		);
+		courierRepository.save(testCourier);
 	}
 
 	@Test
@@ -81,4 +96,22 @@ class AccountControllerTest {
 		boolean result = passwordEncoder.matches("123456789101112", foundedAccount.getPassword());
 		Assertions.assertTrue(result);
 	}
+
+	@Test
+	void 배송기사탈퇴_테스트_성공() throws Exception {
+		DefaultCurrentUser userDetails = new DefaultCurrentUser(
+			testCourier.getId(),
+			testCourier.getEmail(),
+			testCourier.getRole()
+		);
+
+		mockMvc.perform(delete("/api/accounts/retire")
+			.with(SecurityMockMvcRequestPostProcessors.user(userDetails))
+		).andExpect(status().isOk());
+
+		AbstractAccountEntity foundedAccount = accountRepository.findByIdOrThrow(testCourier.getId());
+		Assertions.assertEquals(UserStatus.DELETED,foundedAccount.getStatus());
+	}
+
+
 }
