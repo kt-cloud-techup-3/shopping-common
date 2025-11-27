4395a9a10a0bd51c3469715a7d0dcea28f10ffb2
l-lyun <kimdohyun032@gmail.com>
2025-11-27 15:18:40 +0900

    feat: 이메일 인증코드 발송, 인증 api 작성

---
+++ src/main/java/com/kt/controller/auth/AuthController.java
@@ -0,0 +1,43 @@
+package com.kt.controller.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kt.common.api.ApiResult;
+import com.kt.domain.dto.request.SignupRequest;
+import com.kt.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/email/code")
+	public ResponseEntity<ApiResult<Void>> sendAuthCode(
+		@RequestBody @Valid SignupRequest.SignupEmail request
+	) {
+		log.info("send");
+		System.out.println("asfdsadf");
+		
+		authService.sendAuthCode(request);
+		return ApiResult.ok();
+	}
+
+	@PostMapping("/email/verify")
+	public ResponseEntity<ApiResult<Void>> verifySignupCode(
+		@RequestBody @Valid SignupRequest.VerifySignupCode request
+	) {
+		authService.verifySignupCode(request);
+		return ApiResult.ok();
+	}
+}
