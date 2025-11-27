772f0bbd9bfa9405ca3a730b6810ab22077ca1ca
kf16 <wjdtn747@naver.com>
2025-11-27 21:15:42 +0900

    feat: ReviewController 테스트 기능 추가

---
+++ src/test/java/com/kt/controller/review/ReviewControllerTest.java
@@ -0,0 +1,176 @@
+package com.kt.controller.review;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.kt.common.api.ApiResult;
+import com.kt.constant.Gender;
+import com.kt.constant.OrderProductStatus;
+import com.kt.constant.UserRole;
+import com.kt.domain.dto.request.ReviewRequest;
+import com.kt.domain.dto.response.ReviewResponse;
+import com.kt.domain.entity.CategoryEntity;
+import com.kt.domain.entity.OrderEntity;
+import com.kt.domain.entity.OrderProductEntity;
+import com.kt.domain.entity.ProductEntity;
+import com.kt.domain.entity.ReceiverVO;
+import com.kt.domain.entity.ReviewEntity;
+import com.kt.domain.entity.UserEntity;
+import com.kt.repository.CategoryRepository;
+import com.kt.repository.OrderRepository;
+import com.kt.repository.orderproduct.OrderProductRepository;
+import com.kt.repository.product.ProductRepository;
+import com.kt.repository.review.ReviewRepository;
+import com.kt.repository.user.UserRepository;
+
+import java.util.List;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+class ReviewControllerTest {
+
+	@Autowired
+	ReviewRepository reviewRepository;
+	@Autowired
+	OrderProductRepository orderProductRepository;
+	@Autowired
+	ProductRepository productRepository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	OrderRepository orderRepository;
+	@Autowired
+	CategoryRepository categoryRepository;
+	@Autowired
+	MockMvc mockMvc;
+	@Autowired
+	ObjectMapper objectMapper;
+
+	OrderProductEntity testOrderProduct;
+	ProductEntity testProduct;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		orderProductRepository.deleteAll();
+		userRepository.deleteAll();
+		productRepository.deleteAll();
+		orderRepository.deleteAll();
+		reviewRepository.deleteAll();
+		categoryRepository.deleteAll();
+
+		UserEntity user = UserEntity.create(
+			"주문자테스터1",
+			"wjd123@naver.com",
+			"1234",
+			UserRole.MEMBER,
+			Gender.MALE,
+			LocalDate.now(),
+			"010-1234-5678"
+		);
+		userRepository.save(user);
+
+		ReceiverVO receiver = new ReceiverVO(
+			"수신자테스터1",
+			"010-1234-5678",
+			"강원도",
+			"원주시",
+			"행구로",
+			"주소설명"
+		);
+
+		OrderEntity order = OrderEntity.create(
+			receiver,
+			user
+		);
+		orderRepository.save(order);
+
+		CategoryEntity category = CategoryEntity.create("카테고리", null);
+		categoryRepository.save(category);
+
+		testProduct = ProductEntity.create(
+			"테스트상품명",
+			1000L,
+			5L,
+			category
+		);
+		productRepository.save(testProduct);
+
+		testOrderProduct = new OrderProductEntity(
+			5L,
+			5000L,
+			OrderProductStatus.CREATED,
+			order,
+			testProduct
+		);
+		orderProductRepository.save(testOrderProduct);
+	}
+
+	@Test
+	void 상품리뷰조회_성공() throws Exception {
+		ReviewEntity review = ReviewEntity.create("테스트리뷰내용");
+		review.mapToOrderProduct(testOrderProduct);
+		reviewRepository.save(review);
+
+		MockHttpServletResponse response = mockMvc.perform(get("/api/reviews")
+				.with(SecurityMockMvcRequestPostProcessors.user("wjd123@naver.com"))
+			.param("productId",testOrderProduct.getProduct().getId().toString())
+		).andExpect(status().isOk())
+			.andReturn()
+			.getResponse();
+
+		String json = response.getContentAsString();
+
+		ApiResult<List<ReviewResponse.Search>> reviewResponse = objectMapper
+			.readValue(
+			json,
+			new TypeReference<ApiResult<List<ReviewResponse.Search>>>(){}
+		);
+		ReviewResponse.Search search = reviewResponse.getData().getFirst();
+
+		Assertions.assertNotNull(search);
+		Assertions.assertEquals(search.content(),review.getContent());
+	}
+
+	@Test
+	void 상품리뷰작성_성공() throws Exception {
+		ReviewRequest.Create createDto = new ReviewRequest.Create(
+			testOrderProduct.getId(),
+			"생성한테스트리뷰내용"
+		);
+
+		String json = objectMapper.writeValueAsString(createDto);
+
+		mockMvc.perform(post("/api/reviews")
+			.with(SecurityMockMvcRequestPostProcessors.user("wjd123@naver.com"))
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(json)
+		).andExpect(status().isOk());
+
+		ReviewEntity foundedReview = reviewRepository.findByOrderProductIdOrThrow(testOrderProduct.getId());
+
+		Assertions.assertNotNull(foundedReview);
+		Assertions.assertEquals(createDto.content(), foundedReview.getContent());
+	}
+}
