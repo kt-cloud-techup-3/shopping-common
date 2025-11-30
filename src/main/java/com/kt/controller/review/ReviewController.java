bdc777ed22fa74f48feee279e4449a4ca3339537
junsang-ee <dkwnstkddk@gmail.com>
2025-11-30 17:21:05 +0900

    refactor: Api 공통 필수 누락 부분 추가

--- src/main/java/com/kt/controller/review/ReviewController.java
+++ src/main/java/com/kt/controller/review/ReviewController.java
@@ -4,20 +4,17 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kt.common.api.ApiResult;
-import com.kt.domain.dto.request.ReviewRequest;
 import com.kt.domain.dto.response.ReviewResponse;
 import com.kt.service.ReviewService;
 import java.util.List;
-
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import static com.kt.common.api.ApiResult.wrap;
 
 @RestController
 @RequestMapping("/api/reviews")
@@ -29,7 +26,6 @@ public class ReviewController {
 	public ResponseEntity<ApiResult<List<ReviewResponse.Search>>> search(
 		@RequestParam UUID productId
 	){
-			List<ReviewResponse.Search> reviewDetail = reviewService.getReviewByProductId(productId);
-			return ApiResult.ok(reviewDetail);
+			return wrap(reviewService.getReviewByProductId(productId));
 	}
 }
